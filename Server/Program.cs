e6ed0ac6d62abf999a30030b42369dd5c2f108f0
matvei <matveikylosov@gmail.com>
2024-11-23 09:27:55 +0500

    Были реализованы методы

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +14,37 @@ namespace Server
         public static List<User> Users = new List<User>();
         public static IPAddress IpAddress;
         public static int Port;
+
+        public static bool AutorizationUser(string login, string password)
+        {
+            User user = null;
+            user = Users.Find(x => x.login == login && x.password == password);
+
+            return user != null;
+        }
+
+        public static List<string> GetDirectory(string src)
+        {
+            List<string> FoldersFiles = new List<string>();
+            if (Directory.Exists(src))
+            {
+                string[] dirs = Directory.GetDirectories(src);
+                foreach (string dir in dirs)
+                {
+                    string NameDirectory = dir.Replace(src, "");
+                    FoldersFiles.Add(NameDirectory + "/");
+                }
+
+                string[] files = Directory.GetFiles(src);
+                foreach (string file in files)
+                {
+                    string NameFile = file.Replace(src, "");
+                    FoldersFiles.Add($"{NameFile}");
+                }
+            }
+
+            return FoldersFiles;
+        }
 
         static void Main(string[] args)
         {
