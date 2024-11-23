f884beee071cce9fb52479b7035636d736fadfb0
matvei <matveikylosov@gmail.com>
2024-11-23 11:08:47 +0500

    Добавлены переменные в клиент

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        public static IPAddress IpAddress;
+        public static int Port;
+        public static int Id = -1;
+
+        
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
