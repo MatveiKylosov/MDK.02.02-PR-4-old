27910d22681d958bd4bbbae3e8bf1368d12e3791
matvei <matveikylosov@gmail.com>
2024-11-23 09:18:13 +0500

    были добавлены переменные

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,10 @@ namespace Server
 {
     internal class Program
     {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IpAddress;
+        public static int Port;
+
         static void Main(string[] args)
         {
         }
