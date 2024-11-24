60c3fac3ce6e9bb57e76a20fbedf1979e1001715
matvei <matveikylosov@gmail.com>
2024-11-24 14:23:41 +0500

    Страничка FTP

---
+++ ClientWPF/Pages/FTPPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientWPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для FTPPage.xaml
+    /// </summary>
+    public partial class FTPPage : Page
+    {
+        public FTPPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChangeDirectory_Server(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChangeDirectory_Client(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
